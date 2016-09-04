bcb6d6a551efe488f5ce668cb1497d798c08ea5c
ashley-desouza <ashley.desouza@ischool.berkeley.edu>
2016-09-03 17:24:07 -0700

    Added ids for the todo items.

--- server.js
+++ server.js
@@ -7,18 +7,24 @@ const PORT = process.env.PORT || 3000;
 
 // List of Todos
 let todos = [{
+	id: 1,
 	description: 'Get Started',
 	completed: true
 }, {
+	id: 2,
 	description: 'Clear Things',
 	completed: false
 }, {
+	id: 3,
 	description: 'Restart',
 	completed: false
-];
+}];
 
 app.get('/', (req, res) => {
-	res.send('<h1>Todo App.</h1>');
+	// res.writeHead(200, {'Content-type': 'text/html'});
+	// res.write(JSON.stringify(todos));
+	// res.end('<h1>Todo App.</h1>');
+	res.json(todos);
 });
 
 app.listen(PORT, () => {
