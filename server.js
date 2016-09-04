4475000d4f7f3e0ea271ff4501c2a9e7bed56fbe
ashley-desouza <ashley.desouza@ischool.berkeley.edu>
2016-09-03 20:08:05 -0700

    Added DELTE and PUT options.

--- server.js
+++ server.js
@@ -27,24 +27,81 @@ app.get('/todos/:id', (req, res) => {
 	let todoId = parseInt(req.params.id, 10);
 	let matchedTodo;
 
+	// http://underscorejs.org/#findWhere
 	matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
 		res.send(matchedTodo);
 	} else {
-		res.status(404).send();
+		res.status(404).json({"error": `No todo item found with Id: ${todoId}`});
 	}
 });
 
 // POST a new todo item
 app.post('/todos', (req, res) => {
-	let body = req.body;
+	// Ensure only specific attributes from the body are retreived
+	let body = _.pick(req.body, 'description', 'completed'); //http://underscorejs.org/#pick
+
+	// Data Validation
+	/* 
+		http://underscorejs.org/#isBoolean
+		http://underscorejs.org/#isString
+	*/
+	if (_.isBoolean(body.completed) && _.isString(body.description) && body.description.trim().length) {
+		body.id = todoGlobalId++;
+		body.description = body.description.trim();
+
+		todos.push(body);
+
+		res.json(body);		
+	} else {
+		res.status(400).send();
+	}
 	
-	body.id = todoGlobalId++;
 
-	todos.push(body);
+});
 
-	res.json(body);
+// DELETE a todo item
+app.delete('/todos/:id', (req, res) => {
+	let todoId = parseInt(req.params.id, 10),
+		matchedTodo = todos.filter(todo => todo.id === todoId);
+
+	if (matchedTodo.length) {
+		todos = todos.filter(todo => todo.id !== todoId);
+		 res.json(matchedTodo);		
+	} else {
+		res.status(404).json({"error": `No todo item found with Id: ${todoId}`});
+	}
+});
+
+// PUT for a todo item
+app.put('/todos/:id', (req, res) => {
+	let todoId = parseInt(req.params.id, 10),
+		matchedTodo = todos.filter(todo => todo.id === todoId);
+
+	// Ensure only specific attributes from the body are retreived
+	let body = _.pick(req.body, 'description', 'completed'); //http://underscorejs.org/#pick
+
+	if (!matchedTodo.length) {
+		return res.status(404).json({"error": `No todo item found with Id: ${todoId}`});
+	}
+
+	// Data Validation
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length) {
+		body.description = body.description.trim();	
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		body.completed = body.completed;	
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	// http://underscorejs.org/#extend
+	_.extend(matchedTodo[0], body);
+	res.json(matchedTodo[0]);
 });
 
 app.listen(PORT, () => {
