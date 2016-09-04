97bef24e2022187945de926189933a123620aa22
ashley-desouza <ashley.desouza@ischool.berkeley.edu>
2016-09-03 18:13:18 -0700

    Added body-parser and underscore libraries. Added POST to add new todo item, and GET todo item by id.

--- server.js
+++ server.js
@@ -1,30 +1,50 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('underscore');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+let todoGlobalId = 1;
 
 // List of Todos
-let todos = [{
-	id: 1,
-	description: 'Get Started',
-	completed: true
-}, {
-	id: 2,
-	description: 'Clear Things',
-	completed: false
-}, {
-	id: 3,
-	description: 'Restart',
-	completed: false
-}];
+let todos = [];
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+
+// GET all todo items
+app.get('/todos', (req, res) => {
 	// res.writeHead(200, {'Content-type': 'text/html'});
 	// res.write(JSON.stringify(todos));
 	// res.end('<h1>Todo App.</h1>');
 	res.json(todos);
+});
+
+// GET a todo item by id
+app.get('/todos/:id', (req, res) => {
+	let todoId = parseInt(req.params.id, 10);
+	let matchedTodo;
+
+	matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (matchedTodo) {
+		res.send(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+});
+
+// POST a new todo item
+app.post('/todos', (req, res) => {
+	let body = req.body;
+	
+	body.id = todoGlobalId++;
+
+	todos.push(body);
+
+	res.json(body);
 });
 
 app.listen(PORT, () => {
